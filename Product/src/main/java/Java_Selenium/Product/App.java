015499144c35d0f1420652da5d4859e63c0ee362
Ninad <Ninad@DESKTOP-FQUCIH5>
2023-12-07 23:15:36 +0530

    New commit

--- Product/src/main/java/Java_Selenium/Product/App.java
+++ Product/src/main/java/Java_Selenium/Product/App.java
@@ -1,11 +1,15 @@
 package Java_Selenium.Product;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App {
 
 	@Test
 	public void demoTest() {
-
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.k12insight.com/static/k12insight_login.html");
+		driver.close();
 	}
 }
