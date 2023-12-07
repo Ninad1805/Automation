dfadac4174cd49e249ada2dfc5ed489264604e12
Ninad <Ninad@DESKTOP-FQUCIH5>
2023-12-07 22:52:05 +0530

    New commit on 7th December 2023

--- Product/src/main/java/Java_Selenium/Product/App.java
+++ Product/src/main/java/Java_Selenium/Product/App.java
@@ -4,11 +4,9 @@ package Java_Selenium.Product;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println("This will be the suite base");
-    }
+public class App {
+
+	public static void main(String args[]) {
+		System.out.println("New commit on 7th December 2023");
+	}
 }
